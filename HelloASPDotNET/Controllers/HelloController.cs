d5084d1bf2a3155ae4c8a97fbff97f8089b8ce44
Jacque <jacquemueller2468@gmail.com>
2022-10-12 17:21:38 -0500

    exercises

--- HelloASPDotNET/Controllers/HelloController.cs
+++ HelloASPDotNET/Controllers/HelloController.cs
@@ -15,8 +15,16 @@ namespace HelloASPDotNET.Controllers
         [HttpGet]
         public IActionResult Index()
         {
-            string html = "<form method='post' action='/helloworld'>" +
+            string html = "<form method='post'>" +
                 "<input type='text' name='name' />" +
+                "<select name='language'>" +
+                "<option value='english' selected>English</option>" +
+                "<option value='french'>French</option></select>" +
+                "<option value='german'>German</option></select>" +
+                "<option value='italian'>Italian</option></select>" +
+                "<option value='spanish'>Spanish</option></select>" +
+               
+                // ... add any other languages here ...
                 "<input type='submit' value='Greet Me!' />" +
                 "</form>";
 
@@ -30,6 +38,31 @@ namespace HelloASPDotNET.Controllers
         public IActionResult Welcome(string name = "World")
         {
             return Content("<h1>Welcome to my app, " + name + "!</h1>", "text/html");
+        }
+
+        public static string CreateMessage(string name, string language)
+        {
+            string helloTranslation = "";
+            switch (language)
+            {
+                case "french":
+                    helloTranslation = "Bonjour ";
+                    break;
+                case "english":
+                    helloTranslation = "Hello ";
+                    break;
+                case "german":
+                    helloTranslation = "Guten Tag ";
+                    break;
+                case "italian":
+                    helloTranslation = "Salve ";
+                    break;
+                case "spanish":
+                    helloTranslation = "Hola ";
+                    break;
+                    // ... add any other languages here ...
+            }
+            return helloTranslation + name;
         }
     }
 }
